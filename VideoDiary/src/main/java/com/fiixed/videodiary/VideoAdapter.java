0426d605d15db52f1f5ccba9b0c90744c39f32a5
Andrew Bell <fiixed@gmail.com>
2013-11-20 10:18:07 -0600

    optimsied arrayadapter and fixed listview xml to match parent

--- VideoDiary/src/main/java/com/fiixed/videodiary/VideoAdapter.java
+++ VideoDiary/src/main/java/com/fiixed/videodiary/VideoAdapter.java
@@ -17,8 +17,8 @@ public class VideoAdapter extends ArrayAdapter<Video> {
     int mLayoutResourceId;
     Video[] mData = null;
 
-    public VideoAdapter(Context context, int layoutResourceId, Video[] data){
-        super(context,layoutResourceId,data);
+    public VideoAdapter(Context context, int layoutResourceId, Video[] data) {
+        super(context, layoutResourceId, data);
         this.mContext = context;
         this.mLayoutResourceId = layoutResourceId;
         this.mData = data;
@@ -31,27 +31,44 @@ public class VideoAdapter extends ArrayAdapter<Video> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        VideoHolder holder;
+
         View row = convertView;
         //inflate the layout for a single row
         LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-        row = layoutInflater.inflate(mLayoutResourceId, parent, false);
 
-        //get a reference to all the different view elements we wish to update
-        TextView dateView = (TextView)row.findViewById(R.id.listViewDateTimeTextView);
-        TextView tagsView = (TextView)row.findViewById(R.id.listViewTagTextView);
-        ImageView vidImageView = (ImageView)row.findViewById(R.id.vidImageView);
+        if (row == null) { //if the row has not been created before
+            row = layoutInflater.inflate(mLayoutResourceId, parent, false);
+
+            holder = new VideoHolder();
+
+            //get a reference to all the different view elements we wish to update
+            holder.dateView = (TextView) row.findViewById(R.id.listViewDateTimeTextView);
+            holder.tagsView = (TextView) row.findViewById(R.id.listViewTagTextView);
+            holder.vidImageView = (ImageView) row.findViewById(R.id.vidImageView);
+
+            row.setTag(holder);
+        } else {
+            holder = (VideoHolder) convertView.getTag();
+        }
 
         //get the data from the data array
         Video video = mData[position];
 
         //setting the view to the data we need to display
-        dateView.setText(video.mDate);
-        tagsView.setText(video.mTags);
+        holder.dateView.setText(video.mDate);
+        holder.tagsView.setText(video.mTags);
 
         int resId = mContext.getResources().getIdentifier(video.mNameOfImage, "drawable", mContext.getPackageName());
-        vidImageView.setImageResource(resId);
+        holder.vidImageView.setImageResource(resId);
 
         //returning the row view(because this is called getView after all)
         return row;
+    }
+
+    private static class VideoHolder {
+        public TextView dateView;
+        public TextView tagsView;
+        public ImageView vidImageView;
     }
 }
